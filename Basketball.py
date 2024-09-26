9d154d68d746a0a73e71618d1d5a503d94435fcb
Aditya-santosh-Naik <adityanaikhpt@gmail.com>
2024-09-26 10:56:54 +0530

    Basketball.py

--- Basketball.py
+++ Basketball.py
@@ -1,14 +1,16 @@
-goals=int(input())
-size=int(input())
-l=list(map(int,input().split()))
-mx=0
-for i in range(0,len(1)):
-    sub=l[i:i=size]
-    k=1
-    s=0
-    for j in sub:
-        s+=(j*k)
-        k+=1
-        if s>mx:
-            mx=s
-print(mx)
+goals = int(input()) 
+size = int(input())  
+l = list(map(int, input().split()))  
+mx = 0 
+
+for i in range(0, len(l) - size + 1):  
+    sub = l[i: i + size] 
+    k = 1 
+    s = 0 
+    for j in sub: 
+        s += (j * k) 
+        k += 1 
+    if s > mx: 
+        mx = s
+
+print(mx)  
